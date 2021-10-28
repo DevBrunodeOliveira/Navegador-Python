9d9ad1f6de8398d049f98bb2c4f15de110c34c5a
Bruno de Oliveira <devbrunodeoliveira@gmail.com>
2021-10-28 12:11:17 -0300

    add barra de navegação

--- navegador.py
+++ navegador.py
@@ -11,6 +11,38 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
+        # Barra de Navegação
+        toolbar = QToolBar()
+        self.addToolBar(toolbar)
+
+        # Voltar
+        btn_voltar = QAction('<', self)
+        btn_voltar.triggered.connect(self.browser.back)
+        toolbar.addAction(btn_voltar)
+
+        # Avançar
+        btn_avancar = QAction('>', self)
+        btn_avancar.triggered.connect(self.browser.forward)
+        toolbar.addAction(btn_avancar)
+
+        # Atualizar
+        btn_att = QAction('att', self)
+        btn_att.triggered.connect(self.browser.reload)
+        toolbar.addAction(btn_att)
+
+        #Url
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.url_reader)
+        toolbar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.url_update)
+
+    def url_reader(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+    
+    def url_update(self, url):
+        self.url_bar.setText(url.toString())
+
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Navegador Python')
